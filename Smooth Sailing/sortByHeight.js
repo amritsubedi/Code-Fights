7a0b6521e9867651b62c7243de3b7f5be4da732d
Amrit Subedi <31295445+amritsubedi@users.noreply.github.com>
2019-02-21 16:11:34 -0600

    Update and rename sortByHeight to sortByHeight.js

--- Smooth Sailing/sortByHeight.js
+++ Smooth Sailing/sortByHeight.js
@@ -1,5 +1,4 @@
-/****************
-
+/*
 Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees. People can be very tall!
 
 Example
@@ -7,7 +6,7 @@ Example
 For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
 sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
 
-****************/
+*/
 
 
 function sortByHeight(a) {
