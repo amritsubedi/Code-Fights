c838f9c0ff795b233628dc993d30d2e888207bce
Amrit Subedi <31295445+amritsubedi@users.noreply.github.com>
2019-02-21 16:00:44 -0600

    Update and rename almostIncreasingSequence to almostIncreasingSequence.js

--- Edge of the Ocean/almostIncreasingSequence.js
+++ Edge of the Ocean/almostIncreasingSequence.js
@@ -1,4 +1,4 @@
-/******************
+/*
 
 Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence 
 by removing no more than one element from the array.
@@ -19,7 +19,7 @@ almostIncreasingSequence(sequence) = true.
 You can remove 3 from the array to get the strictly increasing sequence [1, 2]. 
 Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
 
-******************/
+*/
 
 
 function almostIncreasingSequence(seq) {
