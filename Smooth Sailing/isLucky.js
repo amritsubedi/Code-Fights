be02787c9e4b369f233db31ca6dbf0c6c7d9bbcc
Amrit Subedi <31295445+amritsubedi@users.noreply.github.com>
2019-02-21 16:11:13 -0600

    Update and rename isLucky to isLucky.js

--- Smooth Sailing/isLucky.js
+++ Smooth Sailing/isLucky.js
@@ -1,4 +1,4 @@
-/********************
+/*
 
 Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky if the 
 sum of the first half of the digits is equal to the sum of the second half.
@@ -12,7 +12,7 @@ isLucky(n) = true;
 For n = 239017, the output should be
 isLucky(n) = false.
 
-********************/
+*/
 
 
 
