4ae27ec4e784702e0d25a56f4af0fe8349f30c4f
Amrit Subedi <31295445+amritsubedi@users.noreply.github.com>
2019-02-25 16:39:20 -0600

    Update Bubble-Sort.java

--- Others/Bubble-Sort.java
+++ Others/Bubble-Sort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		
 		int[] array = {6, 1, 4, 8, 7, 3, 8, 9};
-		for (int i = 0; i < array.length-1; i++)  {
+		for (int i = 0; i < array.length - 1; i++)  {
 			for (int j = 0; j < array .length - 1; j++) {
 				if (array[j] > array[j+1]) {
 					int temp = array[j]; 
